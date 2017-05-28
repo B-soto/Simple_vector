3f4da3be53cd436d84ae3852da8d42e61fbbb008
B-soto <brycesoto@gmail.com>
2017-05-28 00:39:41 -0700

    Initial Commit

---
+++ Simple_vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple_vector
+//
+//  Created by bryce soto on 5/28/17.
+//  Copyright © 2017 Bryce Soto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
